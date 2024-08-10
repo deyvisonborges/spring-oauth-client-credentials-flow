7a66b9ed4ccff587126047abe9c5d530757f42a2
dbosborges <dbosborges@rd.com.br>
2024-08-10 08:15:51 -0300

    add collection

--- auth-resource/src/main/java/com/oauth/auth_resource/ResourceServerConfig.java
+++ auth-resource/src/main/java/com/oauth/auth_resource/ResourceServerConfig.java
@@ -7,7 +7,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.oauth2.jwt.JwtDecoder;
 //import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -15,10 +23,38 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
-                .jwt(Customizer.withDefaults()));
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults())
+                );
         return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(
+                jwt -> {
+                    List<String> userRoleAuthorities = jwt.getClaimAsStringList("authorities");
+
+                    if (userRoleAuthorities == null) {
+                        userRoleAuthorities = Collections.emptyList();
+                    }
+
+                    JwtGrantedAuthoritiesConverter scopesConverter = new JwtGrantedAuthoritiesConverter();
+
+                    Collection<GrantedAuthority> scopeAuthorities = scopesConverter.convert(jwt);
+
+                    scopeAuthorities
+                            .addAll(userRoleAuthorities.stream()
+                                    .map(SimpleGrantedAuthority::new)
+                                    .toList());
+
+                    return scopeAuthorities;
+                }
+        );
+
+        return converter;
     }
 //
 //    @Bean
